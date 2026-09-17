a20159ab867edefcbbf0b9389e23f4e59c5f170c
ryanpacheco315 <ryanpacheco315@gmail.com>
2026-09-17 05:23:33 -0400

    Implemented docker to containerize project + implemented database inside of docker

---
+++ project/lcs-server/database/seed-data.sql
@@ -0,0 +1,4 @@
+use live_chat_support;
+
+insert into user (full_name, username, password, role)
+values ('System Admin', 'admin', '$2b$10$jBQr8dzSuPZEHt/HCJ64KOG288oPbHe6Qi38BJdPmWtk2fWHTbuRS', 'ADMIN');
